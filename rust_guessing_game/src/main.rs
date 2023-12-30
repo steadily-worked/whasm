d053a1cee8426243a6c7cf2168bf061ceba44a70
Sangmin Park <iamsangminpark@gmail.com>
2023-12-30 23:45:38 +0900

    feat: ignore when input value is not u32

--- rust_guessing_game/src/main.rs
+++ rust_guessing_game/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     // 범위: start..=end
-    println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -24,7 +23,10 @@ fn main() {
             // 입력값을 `mut guess` 변수에 저장함. `&`는 이 인수가 참조(reference)임을 나타냄.
             .expect("Failed to read line"); // 프로그램이 충돌을 일으킬 경우 expect 내의 텍스트를 보여줌. 자바스크립트로 따지면 throw new Error("")와 같은 것인듯. expect는 필수값임.
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         // 아래에서 `secret_number`(i32)와 비교하기 위해 `guess` 값을 i32의 형태로 변환.
         // trim(): JS의 trim()과 동일. u32에는 숫자 데이터만 들어갈 수 있으므로 반드시 trim 처리를 해줘야함.
         // i32 vs u32: number type이라는 점은 동일하나, i32는 음수도 가능한 반면 u32는 양수만 가능함.
