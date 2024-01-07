4e89f1afe8472638c012bf9d36215ee943752bde
Sangmin Park <iamsangminpark@gmail.com>
2024-01-07 12:31:10 +0900

    feat: slice

--- rust_slice/src/main.rs
+++ rust_slice/src/main.rs
@@ -34,6 +34,7 @@ fn main() {
         let _slice = &s[..];
         // 이 두개는 같은 값이다.
     }
+    other_slices();
 }
 
 // first_word 함수: 공백으로 구분된 단어의 문자열을 받아, 해당 문자열에서 찾은 첫번째 단어를 반환하는 함수. 함수가 문자열에서 공백을 찾지 못하면, 전체 문자열이 한 단어여야 하므로 전체 문자열을 반환해야 함.
@@ -55,7 +56,7 @@ fn first_word(s: &String) -> usize {
 // 즉, 문자열과는 별개의 값이기 때문에 향후에도 여전히 유효하다는 보장이 없다. (이후 4행의 주석을 보기)
 
 // slice를 사용해서 다시 작성한 first_word 함수. slice는 &str의 타입을 가짐.
-fn first_word_with_slice(s: &String) -> &str {
+fn first_word_with_slice(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -65,4 +66,41 @@ fn first_word_with_slice(s: &String) -> &str {
     }
 
     &s[..]
+}
+
+fn slice_literal() {
+    let _s = "Hello, world!";
+    // 여기서 s의 타입은 &str이다: 바이너리의 특정 지점을 가리키는 조각이다.
+    // &str은 immutable reference이다. 그러므로 이 &str의 특정 지점인 스트링 리터럴도 immutable한 것이다.
+
+    // 이렇게 될 경우 first_word와 first_word_with_slice의 파라미터로 전달되는 &String 타입을 대신하여 &str 타입을 적용할 수도 있다.
+    // string slice(str)을 가질 경우 이를 직접 전달할 수 있다. &String을 가질 경우 String의 slice와 String의 참조를 전달할 수 있다.
+    // 이렇게, String을 대신하여 string slice(str)를 받도록 파라미터를 수정하는 경우 기능을 잃지 않으면서도 좀더 API를 일반적이고 유용하게 만들 수 있다.
+
+    let my_string = String::from("hello world!");
+
+    // `first_word_with_slice`(`first_word`)의 파라미터를 `&str`로 변경했을 경우, `String`의 slice에서(부분이든 전체든) 작동한다.
+    let _word = first_word_with_slice(&my_string[0..6]);
+    let _word = first_word_with_slice(&my_string[..]);
+    // `first_word`는 `String`의 참조(`String`의 전체 슬라이스 포함)에서도 작동한다.
+    let _word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+    // `first_word`는 스트링 리터럴의 slice에서도(부분이든 전체든) 작동한다.
+    let _word = first_word_with_slice(&my_string_literal[0..6]);
+    let _word = first_word_with_slice(&my_string_literal[..]);
+
+    // 스트링 리터럴(str)은 이미 스트링의 slice 형태이기 때문에, 아래의 코드 또한 문제없이 작동한다.
+    let _word = first_word_with_slice(my_string_literal);
+
+    // 정리하면, 파라미터로 들어가는 &String 타입이 하는 모든 일을 &str 타입도 할 수 있기 때문에, &String 타입으로 지정하는 것보다 &str 타입으로 지정하는 것이 더 flexible하다.
+}
+
+fn other_slices() {
+    // string slice는 당연하게도 string 타입에 국한되어있다.
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
+    // slice의 타입은 &[i32]이다.
+    println!("slice is: {:?}", slice); // slice는 [2, 3]이 나온다.
 }
