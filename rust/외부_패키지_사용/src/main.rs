59a4e8d267799673f148e3ab60c4b4c0bf7b2046
Sangmin Park <iamsangminpark@gmail.com>
2024-04-29 09:19:57 +0900

    외부 패키지

--- rust/외부_패키지_사용/src/main.rs
+++ rust/외부_패키지_사용/src/main.rs
@@ -2,16 +2,16 @@ use rand::Rng; // Cargo.toml에 추가해야함.
                // std 표준 라이브러리도 마찬가지로 외부 크레이트지만, 러스트 언어에 포함되어있으므로 Cargo.toml에 추가할 필요가 없다.
                // 그래도, 여전히 `use std::collections::HashMap;` 이런식으로 사용해야한다.
 
-use std::assert;
-use std::cmp::Ordering;
+// use std::assert;
+// use std::cmp::Ordering;
 // 이 두가지는 아래의 한가지로 바꿀 수 있다.
-// use std::{cmp::Ordering, assert};
+use std::{assert, cmp::Ordering};
 
-use std::io;
-use std::io::Write;
+// use std::io;
+// use std::io::Write;
 // 이 두가지는 아래의 한가지로 바꿀 수 있다. 그 자체는 self가 된다.
 // 근데, 왜 그 자체를 불러오면서 Write도 불러오는걸까? 이런 케이스가 많을까?
-// use std::io::{self, Write};
+use std::io::{self, Write};
 
 // use std::collections::*;
 // 경로 내에 정의된 모든 공개 아이템을 가져올 수 있다.
