0c248d81d7fd36a5aaa7d7b34dc4693d60d6eb3b
steadily-worked <iamsangminpark@gmail.com>
2024-10-23 16:24:15 +0900

    finish api-server, formatting

--- rust/api-server/src/main.rs
+++ rust/api-server/src/main.rs
@@ -1,3 +1,9 @@
+use error::Error;
+use router::create_router;
+
+mod api;
+mod error;
+mod router;
 mod todo;
 
 #[tokio::main]
@@ -9,14 +15,19 @@ async fn main() {
 
     let bind_addr = std::env::var("BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:3000".to_string()); // BIND_ADDR 환경변수에서 주소를 가져오거나, 없으면 127.0.0.1:3000을 기본으로 설정
 
-    axum::Server::bind(&bind_addr.parse().unwrap()).serve(router.into_make_service()).await.expect("unable to start server")
+    axum_server::bind(bind_addr.parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .expect("unable to start server")
     // BIND_ADDR를 소켓 주소로 파싱. 서비스 생성 후 HTTP 서버 시작
 }
 
-fn init_tracing() { // 추적 초기화 함수
+fn init_tracing() {
+    // 추적 초기화 함수
     use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
 
-    let rust_log = std::env::var(EnvFilter::DEFAULT_ENV).unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".to_string());
+    let rust_log = std::env::var(EnvFilter::DEFAULT_ENV)
+        .unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".to_string());
     // RUST_LOG 환경변수를 가져오고, 없으면 기본값을 제공함.
     // tower_http::*에 대한 디버그 수준 메시지, sqlx::*에 대한 정보 수준 메시지를 포함함.
 
@@ -25,33 +36,32 @@ fn init_tracing() { // 추적 초기화 함수
         .with(
             EnvFilter::builder() // 환경변수 기반 필터 생성. 기본 로그 수준을 info로 설정하고 그렇지 않으면 RUST_LOG에서 제공하는 값을 사용함.
                 .with_default_directive(LevelFilter::INFO.into())
-                .parse_lossy(rust_log)
+                .parse_lossy(rust_log),
         )
         .init();
 }
 
-// 연결 pool을 사용해서 DB에 대한 연결을 얻음. connection pool 사용 시 각 요청에 대한 새로운 연결을 생성할 필요가 없이 
+// 연결 pool을 사용해서 DB에 대한 연결을 얻음. connection pool 사용 시 각 요청에 대한 새로운 연결을 생성할 필요가 없이
 // DB에 대한 기존 연결을 얻어 재사용할 수 있음.
-async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> { 
+async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, Error> {
     use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
     use std::str::FromStr;
 
-    let db_connection_str = std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
+    let db_connection_str =
+        std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
     // DATABASE_URL 환경변수에서 데이터베이스 연결 문자열을 가져오거나, 없으면 sqlite:db.sqlite를 기본으로 설정
     // 이 경우 현재 디렉토리 내 `db.sqlite` 라는 파일이 열림.
 
-    let dbpool = SqlitePoolOptions::new().connect_with(
-        SqliteConnectOptions::from_str(&db_connection_str)?
-        .create_if_missing(true)) // DB가 없을 때 드라이버에 DB를 생성하도록 요청
+    let dbpool = SqlitePoolOptions::new()
+        .connect_with(SqliteConnectOptions::from_str(&db_connection_str)?.create_if_missing(true)) // DB가 없을 때 드라이버에 DB를 생성하도록 요청
         .await
         .expect("can't connect to DB");
 
-    sqlx::migrate!()// DB 연결 후 마이그레이션 실행
+    sqlx::migrate!() // DB 연결 후 마이그레이션 실행
         .run(&dbpool) // 새로 생성한 dbpool을 SQLx에 직접 전달
         .await
         .expect("can't run migrations");
-    // 데이터베이스 마이그레이션 실행  
+    // 데이터베이스 마이그레이션 실행
 
     Ok(dbpool)
 }
-
---
+++ rust/api-server/src/router.rs
@@ -0,0 +1,24 @@
+use axum::{routing::get, Router};
+use sqlx::{Pool, Sqlite};
+
+pub async fn create_router(dbpool: Pool<Sqlite>) -> Router {
+    use crate::api::{ping, todo_create, todo_delete, todo_list, todo_read, todo_update};
+    use tower_http::cors::{Any, CorsLayer};
+    use tower_http::trace::TraceLayer;
+
+    Router::new()
+        .route("/alive", get(|| async { "ok" }))
+        .route("/ready", get(ping))
+        .nest(
+            "/v1",
+            Router::new()
+                .route("/todos", get(todo_list).post(todo_create))
+                .route(
+                    "/todos/:id",
+                    get(todo_read).put(todo_update).delete(todo_delete),
+                ),
+        )
+        .with_state(dbpool)
+        .layer(CorsLayer::new().allow_methods(Any).allow_origin(Any))
+        .layer(TraceLayer::new_for_http())
+}
